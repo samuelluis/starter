39465c6f0416bbfac91eb5f332eb47986d67734f
Samuel Luis De La Cruz <sammultiservices@gmail.com>
2013-03-26 17:21:30 -0400

    Quick Start

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+	<head>
+		<title>Starter</title>
+		<!-- Style Sheets -->
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+
+		<!-- Java Scripts -->
+		<script type="text/javascript" src="js/jquery.js"></script>
+		<script type="text/javascript" src="js/jqueryui.js"></script>
+		<script type="text/javascript" src="js/base.js"></script>
+	</head>
+	<body class="container_16">
+		<div id="connection" class="grid_8 push_4">
+			<fieldset>
+				<div id="connection_server_field">
+					<label>
+						<span>Server:</span>
+						<input id="connection_server" />
+					</label>
+				</div>
+			</fieldset>
+		</div>
+	</body>
+</html>
